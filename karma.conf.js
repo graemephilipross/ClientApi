1284d656d9a09f0364e7cc2c3720c4b7d96dc820
Graeme Ross <graeme@oneutilitybill.co>
2017-08-18 12:31:23 +0100

    karma-coverage-istanbul-reporter used

--- karma.conf.js
+++ karma.conf.js
@@ -26,12 +26,10 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
-    coverageReporter: {
-      reporters: [
-        { type: 'text' },
-        { type: 'text-summary' }
-      ]
+    reporters: ['progress', 'coverage-istanbul'],
+    coverageIstanbulReporter: {
+      reports: ['text'],
+      fixWebpackSourcePaths: true
     },
     // web server port
     port: 9876,
